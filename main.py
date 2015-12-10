0f3cf85f70bff6086327a324190da1526a6ceea6
Mike <Svaazz@users.noreply.github.com>
2015-12-11 00:39:29 +0100

    Uploaded main.py
    Interfaz

---
+++ main.py
@@ -0,0 +1,47 @@
+#By Svaazz
+
+import os
+import bone
+sitio = ""
+
+def main(direccion):
+	global archivo
+	os.system("clear")
+	print("############By Svaazz#############")
+	print("##################################")
+	print("0. Salir.")
+	print("1. Buscar cadena de caracteres.")
+	print("2. Guardar contenido en el disco.")
+	print("3. Cambiar url.")
+	print("##################################")
+	eleccion = input(">> ")
+	if eleccion == "1":
+		bone.buscar(direccion)
+		main(direccion)
+	elif eleccion == "2":
+		print("Introduce un nombre para el archivo:")
+		archivo = input(str(">> "))
+		if archivo == "":
+			archivo = "Contenido.txt"
+		print("El archivo ", archivo, " esta siendo creado")
+		bone.conectar(direccion, archivo)
+		main(direccion)
+	elif eleccion == "3":
+		url()
+	elif eleccion == "0":
+		os.system("clear")
+		print("Saliendo...")
+		exit()
+	else:
+		main(direccion)
+
+def url():
+	os.system("clear")
+
+	print("Por favor introduce una URL: (http://ejemplo.com)")
+	sitio = input(str(">> "))
+	if sitio.startswith("http"):
+		main(sitio)
+	else:
+		url()
+url()
